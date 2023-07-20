1a153ae2eef762fc0a8354fd587b627724aca564
Shin-sangwon <dnjsml10@gmail.com>
2023-07-20 20:53:21 +0900

    chore : project init

---
+++ src/main/java/com/refill/RefillApplication.java
@@ -0,0 +1,13 @@
+package com.refill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/refill/RefillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
