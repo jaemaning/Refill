0173982660a763d28d0b09dad27a3bc4cd93f7a3
Shin-sangwon <dnjsml10@gmail.com>
2023-07-23 20:38:59 +0900

    refactor : UserDetailsService 수정

--- backend/Refill/src/main/java/com/refill/hospital/repository/HospitalRepository.java
+++ backend/Refill/src/main/java/com/refill/hospital/repository/HospitalRepository.java
@@ -1,8 +1,11 @@
 package com.refill.hospital.repository;
 
 import com.refill.hospital.entity.Hospital;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+
+    Optional<Hospital> findByLoginId(String loginId);
 
 }
--- backend/Refill/src/main/java/com/refill/security/service/UserDetailServiceImpl.java
+++ backend/Refill/src/main/java/com/refill/security/service/UserDetailServiceImpl.java
@@ -1,6 +1,8 @@
 package com.refill.security.service;
 
 import com.refill.global.exception.ErrorCode;
+import com.refill.hospital.entity.Hospital;
+import com.refill.hospital.repository.HospitalRepository;
 import com.refill.member.exception.MemberException;
 import com.refill.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +16,18 @@ import org.springframework.stereotype.Service;
 public class UserDetailServiceImpl implements UserDetailsService {
 
     private final MemberRepository memberRepository;
-
+    private final HospitalRepository hospitalRepository;
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+
         return memberRepository.findByLoginId(loginId)
+            .map(UserDetails.class::cast)
+            .orElse(hospitalRepository.findByLoginId(loginId)
                                .orElseThrow(
                                    () -> new MemberException(
                                        ErrorCode.USERNAME_NOT_FOUND.getCode(),
                                        ErrorCode.USERNAME_NOT_FOUND,
                                        ErrorCode.USERNAME_NOT_FOUND.getMessage()
-                                   ));
+                                   )));
     }
 }
