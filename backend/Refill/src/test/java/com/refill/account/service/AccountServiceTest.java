e18fa0c0f264d4c19f72e3491a40c309051094e5
Shin-sangwon <dnjsml10@gmail.com>
2023-07-25 09:42:54 +0900

    feat : service test util class 작성

---
+++ backend/Refill/src/test/java/com/refill/account/service/AccountServiceTest.java
@@ -0,0 +1,9 @@
+package com.refill.account.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.refill.util.ServiceTest;
+
+class AccountServiceTest extends ServiceTest {
+
+}
