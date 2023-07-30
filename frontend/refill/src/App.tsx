7d42a77f8880dbfd564253918eb6506d0590336b
Kim SeoungHyeon <kim0221_@naver.com>
2023-07-30 20:20:42 +0900

    feat : 공통 Navbar, Footer 생성

--- frontend/refill/src/App.tsx
+++ frontend/refill/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Counter from './components/Counter';
-import nav_logo from './assets/logo_final.png';
+
 // import LoginForm from './LoginForm'
 import MemberJoin from './MemberJoin'
 import ButtonTest from './components/ButtonTest'
@@ -9,15 +11,7 @@ import ButtonTest from './components/ButtonTest'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <nav className="App-navbar flex">
-        <img src={nav_logo} alt="nav_log" />
-        <div className="flex align-items-center">
-          <a href="/">예약</a>
-          <a href="/">서비스</a>
-          <a href="/">로그인</a>
-          <a href="/">회원가입</a>
-        </div>
-      </nav>
+      <Navbar />
       <div className="App-body">
         {/* <h1>Hello Refill!</h1>
         <h3>test for redux ! </h3> */}
@@ -25,7 +19,9 @@ const App: React.FC = () => {
         <MemberJoin />
         <Counter />
         <ButtonTest/>
+        
       </div>
+      <Footer />
     </div>
   );
 }
