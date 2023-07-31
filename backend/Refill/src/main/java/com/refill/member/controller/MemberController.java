0b772a92f49a0227083250552d967817c5354746
Shin-sangwon <dnjsml10@gmail.com>
2023-07-31 13:39:38 +0900

    feat : member 개인정보 수정 기능 구현

--- backend/Refill/src/main/java/com/refill/member/controller/MemberController.java
+++ backend/Refill/src/main/java/com/refill/member/controller/MemberController.java
@@ -1,14 +1,19 @@
 package com.refill.member.controller;
 
+import com.refill.member.dto.request.MemberInfoUpdateRequest;
 import com.refill.member.dto.response.MemberInfoResponse;
 import com.refill.member.service.MemberService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,6 +30,15 @@ public class MemberController {
         MemberInfoResponse memberInfoResponse = memberService.getMemberByLoginId(loginId);
 
         return ResponseEntity.ok().body(memberInfoResponse);
+    }
+
+    @PutMapping("/mypage")
+    public ResponseEntity<String> modifyMemberInfo(@AuthenticationPrincipal String loginId, @RequestPart("memberInfoUpdateRequest") @Valid final MemberInfoUpdateRequest memberInfoUpdateRequest, @RequestPart(value = "profileImg", required = false) MultipartFile profileImg){
+
+        log.debug("'{}' member request information update", loginId);
+        memberService.modifyMember(loginId, memberInfoUpdateRequest, profileImg);
+
+        return ResponseEntity.noContent().build();
     }
 
 
--- backend/Refill/src/main/java/com/refill/member/service/MemberService.java
+++ backend/Refill/src/main/java/com/refill/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.refill.member.service;
 
 import com.refill.global.exception.ErrorCode;
+import com.refill.global.service.AmazonS3Service;
+import com.refill.member.dto.request.MemberInfoUpdateRequest;
 import com.refill.member.dto.response.MemberInfoResponse;
 import com.refill.member.entity.Member;
 import com.refill.member.exception.MemberException;
@@ -11,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final AmazonS3Service amazonS3Service;
     private final JwtProvider jwtProvider;
 
     @Value("${jwt.token.secret}")
@@ -58,12 +62,23 @@ public class MemberService {
                                .orElseThrow(() -> new MemberException(ErrorCode.USERNAME_NOT_FOUND));
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public MemberInfoResponse getMemberByLoginId(String loginId) {
 
         Member member = findByLoginId(loginId);
 
         return new MemberInfoResponse(member);
     }
+    @Transactional
+    public void modifyMember(String loginId, MemberInfoUpdateRequest memberInfoUpdateRequest, MultipartFile profileImg) {
 
+        Member member = findByLoginId(loginId);
+        member.update(memberInfoUpdateRequest);
+
+        if(profileImg != null) {
+            String profileAddress = amazonS3Service.uploadFile(profileImg);
+            member.updateFileAddress(profileAddress);
+        }
+
+    }
 }
