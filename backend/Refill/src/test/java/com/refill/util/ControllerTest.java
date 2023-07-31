20d5ed0e7dea2d9e1fefb0dee8287a69a1dcfa7c
Shin-sangwon <dnjsml10@gmail.com>
2023-07-31 16:23:34 +0900

    test : memberController testcode 구현

--- backend/Refill/src/test/java/com/refill/util/ControllerTest.java
+++ backend/Refill/src/test/java/com/refill/util/ControllerTest.java
@@ -5,10 +5,18 @@ import com.refill.account.controller.AccountController;
 import com.refill.account.service.AccountService;
 import com.refill.hospital.repository.HospitalRepository;
 import com.refill.hospital.service.HospitalService;
+import com.refill.member.controller.MemberController;
 import com.refill.member.repository.MemberRepository;
 import com.refill.member.service.MemberService;
 import com.refill.security.util.JwtProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,7 +28,8 @@ import org.springframework.test.web.servlet.MockMvc;
 @ActiveProfiles("test")
 @MockBean(JpaMetamodelMappingContext.class)
 @WebMvcTest({
-    AccountController.class
+    AccountController.class,
+    MemberController.class
 })
 public class ControllerTest {
 
@@ -30,6 +39,9 @@ public class ControllerTest {
 
     @MockBean protected JwtProvider jwtProvider;
 
+    @Value("${jwt.token.secret}")
+    protected String secretKey;
+
     // Service
     @MockBean protected AccountService accountService;
     @MockBean protected MemberService memberService;
@@ -38,4 +50,27 @@ public class ControllerTest {
     // Repository
     @MockBean protected MemberRepository memberRepository;
     @MockBean protected HospitalRepository hospitalRepository;
+
+    protected String createToken(String loginId, String secretKey) {
+
+        final long accessTokenExpireTimeMs = 3600000L; // 1시간
+        final long refreshTokenExpireTimeMs = 1209600000L; // 2주일
+
+        Claims claims = Jwts.claims();
+        claims.put("loginId", loginId);
+
+        Date now = new Date();
+        Date accessTokenExpiration = new Date(now.getTime() + accessTokenExpireTimeMs);
+        Date refreshTokenExpiration = new Date(now.getTime() + refreshTokenExpireTimeMs);
+
+        return Jwts.builder()
+                   .setClaims(claims)
+                   .setIssuedAt(now)
+                   .setExpiration(accessTokenExpiration)
+                   .signWith(
+                                     Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)),
+                                     SignatureAlgorithm.HS256)
+                   .compact();
+
+    }
 }
