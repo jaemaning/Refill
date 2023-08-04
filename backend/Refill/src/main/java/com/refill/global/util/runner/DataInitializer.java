7d5cfb90e436183168b24cee7ac42393968a98f8
apicad0307 <apicad0307@naver.com>
2023-08-04 15:46:26 +0900

    chore: 병원 위도 경도 수정

--- backend/Refill/src/main/java/com/refill/global/util/runner/DataInitializer.java
+++ backend/Refill/src/main/java/com/refill/global/util/runner/DataInitializer.java
@@ -102,8 +102,8 @@ public class DataInitializer implements CommandLineRunner {
                                         .tel("02-2345-3465")
                                         .hospitalBannerImg("https://picsum.photos/600/600/?random")
                                         .hospitalProfileImg("https://picsum.photos/600/600/?random")
-                                        .latitude(BigDecimal.valueOf(33.452615))
-                                        .longitude(BigDecimal.valueOf(126.570890))
+                                        .latitude(BigDecimal.valueOf(33.452613 + 0.000001 * i))
+                                        .longitude(BigDecimal.valueOf(126.570888 + 0.000001 * i))
                                         .postalCode(
                                             String.valueOf(random.nextInt(90000)+10000))
                                         .registrationImg("https://picsum.photos/600/600/?random")
