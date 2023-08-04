94b30506e41ac8c78d1b1fe8a168282764178c2e
Shin-sangwon <dnjsml10@gmail.com>
2023-08-04 12:10:42 +0900

    test : reservationControllerTest 토대 구현

---
+++ backend/Refill/src/test/java/com/refill/reservation/controller/ReservationControllerTest.java
@@ -0,0 +1,9 @@
+package com.refill.reservation.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.refill.util.ControllerTest;
+
+class ReservationControllerTest extends ControllerTest {
+
+}
