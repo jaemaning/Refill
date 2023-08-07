a6ae54539e4b550cc6eaa246c72b9dc353a8576f
apicad0307 <apicad0307@naver.com>
2023-08-07 20:02:33 +0900

    feat: 리뷰 신고 조회를 위한 dto 생성

---
+++ backend/Refill/src/main/java/com/refill/admin/dto/response/ReportReviewResponse.java
@@ -0,0 +1,27 @@
+package com.refill.admin.dto.response;
+
+import com.refill.global.entity.UserInfo;
+import com.refill.report.entity.Report;
+import com.refill.review.dto.response.ReviewResponse;
+import com.refill.review.entity.Review;
+import javax.validation.constraints.NotNull;
+
+public record ReportReviewResponse (
+    @NotNull Long reportId,
+    @NotNull ReviewResponse reviewResponse,
+    @NotNull String reporterType, // 신고한 사람의 유형: MEMBER, HOSPITAL (의사는 실제 로그인하는 회원이 아님)
+    @NotNull Long reporterId,
+    @NotNull String reporterName,
+    @NotNull String content
+    ){
+    public ReportReviewResponse(Report report, UserInfo userInfo, Review review) {
+        this(
+            report.getId(),
+            new ReviewResponse(review),
+            report.getReporterType().name(),
+            report.getReporterId(),
+            userInfo.getName(),
+            report.getContent()
+        );
+    }
+}
