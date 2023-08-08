8bb911f6ae80d95e68990c83782dfb7f822aeedd
Kim SeoungHyeon <kim0221_@naver.com>
2023-08-08 13:50:28 +0900

    feat : Token_test

--- frontend/refill/src/store/reducers/index.ts
+++ frontend/refill/src/store/reducers/index.ts
@@ -1,8 +1,19 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import selectedReducer from "./selectedReducer";
+import loginReducer from "./loginReducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   selected: selectedReducer,
+  login: loginReducer,
 });
 
-export default rootReducer;
+const persistConfig = {
+  key: "login",
+  storage,
+  whitelist: ["login"],
+};
+
+export default persistReducer(persistConfig, rootReducer);
+export type RootState = ReturnType<typeof rootReducer>;
