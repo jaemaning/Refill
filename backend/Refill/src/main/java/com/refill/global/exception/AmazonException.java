3780d288cd48378254a24500c39822a90e93f9ae
신상원 <dnjsml30@naver.com>
2023-08-09 11:17:17 +0900

    refactor : emailService template 사용하게 변경

--- backend/Refill/src/main/java/com/refill/global/exception/AmazonException.java
+++ backend/Refill/src/main/java/com/refill/global/exception/AmazonException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class AmazonS3Exception extends RuntimeException{
+public class AmazonException extends RuntimeException{
 
     private ErrorCode errorCode;
 }
