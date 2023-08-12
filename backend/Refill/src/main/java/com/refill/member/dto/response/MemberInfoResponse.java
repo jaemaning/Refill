4decfc48f5656c1f09e770544fcfe0dc7a419d06
신상원 <dnjsml30@naver.com>
2023-08-12 21:31:09 +0900

    refactor : 회원 마이페이지

--- backend/Refill/src/main/java/com/refill/member/dto/response/MemberInfoResponse.java
+++ backend/Refill/src/main/java/com/refill/member/dto/response/MemberInfoResponse.java
@@ -1,7 +1,11 @@
 package com.refill.member.dto.response;
 
+import com.refill.aidiagnosis.dto.response.AiDiagnosisListResponse;
+import com.refill.consulting.dto.response.ConsultingListResponse;
 import com.refill.member.entity.Member;
+import com.refill.reservation.dto.response.ReservationListResponse;
 import java.time.LocalDate;
+import java.util.List;
 
 public record MemberInfoResponse(
 
@@ -11,7 +15,12 @@ public record MemberInfoResponse(
     String tel,
     String nickname,
     String email,
-    String profileImg
+    String profileImg,
+
+    List<AiDiagnosisListResponse> aiDiagnosisList,
+    List<ReservationListResponse> reservationList,
+    List<ConsultingListResponse> consultingList
+
 ) {
 
     public MemberInfoResponse(Member member) {
@@ -22,7 +31,25 @@ public record MemberInfoResponse(
             member.getTel(),
             member.getNickname(),
             member.getEmail(),
-            member.getProfileImg()
+            member.getProfileImg(),
+            null,
+            null,
+            null
+        );
+    }
+
+    public MemberInfoResponse(Member member, List<AiDiagnosisListResponse> aiDiagnosisList, List<ReservationListResponse> reservationList, List<ConsultingListResponse> consultingList) {
+        this (
+            member.getName(),
+            member.getAddress(),
+            member.getBirthDay(),
+            member.getTel(),
+            member.getNickname(),
+            member.getEmail(),
+            member.getProfileImg(),
+            aiDiagnosisList,
+            reservationList,
+            consultingList
         );
     }
 
